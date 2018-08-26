db6778ad76791e34fa68fe3d60a783512c38ed37
tconet <tiagonet@gmail.com>
2018-08-26 19:37:07 -0300

    Primeira Versão

---
+++ src/main/java/br/com/configserver/ServerApplication.java
@@ -0,0 +1,16 @@
+package br.com.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
